742c29166ce1c44c11f491fa6244b179eb9d9803
Cris <bryan@hightechgrafix.com>
2026-02-11 07:55:31 -0600

    the comma.. it burns!

--- warehouse-game/js/config.js
+++ warehouse-game/js/config.js
@@ -35,7 +35,8 @@ const CONFIG = {
   
   //Supabase config
   SUPABASE_URL: 'https://sbfslzwnkztmodnlsigq.supabase.co',
-SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNiZnNsendua3p0bW9kbmxzaWdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3OTQ5NTgsImV4cCI6MjA3ODM3MDk1OH0.ebZ1IvA5FQN7EhIcluJvw3OqMFU4Czkhin_ffTPx9vg'
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNiZnNsendua3p0bW9kbmxzaWdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3OTQ5NTgsImV4cCI6MjA3ODM3MDk1OH0.ebZ1IvA5FQN7EhIcluJvw3OqMFU4Czkhin_ffTPx9vg'
+
   // Helper: Calculate canvas size for a level
   calculateCanvasSize(levelWidth, levelHeight) {
     const gameWidth = levelWidth * this.TILE_SIZE + (this.GRID_PADDING * 2);
